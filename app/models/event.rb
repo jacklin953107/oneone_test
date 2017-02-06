0284d84d801355675bb1746059a1cd0713d8faf8
jacklin953107 <jacklin953107@gmail.com>
2017-02-06 16:07:30 +0800

    One to Many - event and category

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
-  validates_presence_of :name
-  has_many :attendees, :event_groupships
+  #validates_presence_of :name
+  has_many :attendees
+  has_many :event_groupships
   has_many :groups, :through => :event_groupships
   has_one :location
   belongs_to :category
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "events#index"
   #match ':controller(/:action(/:id(.:format)))', :via => :all
-  resources :events
+  resources :events do
+    resources :attendees, :controller => 'event_attendees'
+  end
 end
