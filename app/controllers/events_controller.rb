ab5737e5ec865766e21895067f71cccca6fe41d4
jacklin953107 <jacklin953107@gmail.com>
2017-02-07 14:02:20 +0800

    Edit one to one 2 -- Event and Location

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,7 +53,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit( :name, :description, :category_id, :location_attributes => [])
+    params.require(:event).permit( :name, :description, :category_id, :location_attributes => [:id, :name, :_destroy])
   end
 
   def set_event
